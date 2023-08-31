e88585aea789a1aed5ef12b1dc9f7f2f89e398ed
bigcc218 <bigcc218@hotmail.com>
2023-08-31 14:51:36 +0800

    code

---
+++ src/main/java/top/bigcc218/TurtorialModClient.java
@@ -0,0 +1,10 @@
+package top.bigcc218;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurtorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
